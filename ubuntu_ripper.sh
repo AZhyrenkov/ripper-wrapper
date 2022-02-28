fccfb955f21249392fd00e1644bae83e28b37723
Oleksii Zhyrenkov <ozhyrenkov@gmail.com>
2022-02-28 17:13:40 +0200

    finig typo

--- ubuntu_ripper.sh
+++ ubuntu_ripper.sh
@@ -34,6 +34,7 @@ function install_dependencies {
     curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
     chmod +x /usr/local/bin/docker-compose
   exit 1
+}
 
 function check_params {
   if [ -z ${mode+x} ]; then
